aeb06ff868a326da64490cbd7274a490fc21d677
Hajnal Mate <matehajnal.hajnal@gmail.com>
2023-02-26 00:29:36 +0100

    Updated app.py

--- app.py
+++ app.py
@@ -2,38 +2,40 @@ from flask import Flask, request, render_template
 import openai
 import deepl
 
-openai.api_key = ""
-deepl_key = ""
-
-translator = deepl.Translator(deepl_key)
+openai.api_key = "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"
+auth_key = "f0fdeeed-e503-f96d-3ee1-748b1bb68de1:fx"
+translator = deepl.Translator(auth_key)
 
 app = Flask(__name__)
-
-""" @app.route('/')
-def index():
-    return render_template('index.html') """
 
 @app.route('/')
 def my_form():
     return render_template('my-form.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    
-    response = openai.Completion.create(
+@app.route('/', methods=['POST', 'GET'])
+def my_form_post():                         #TÚL NAGY, DARABOLNI!
+
+    messages = []
+    if request.method == 'POST':
+        message = request.form['text']
+        
+        response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=text,
+        prompt=message,
         temperature=0.6,
         max_tokens=150,
         top_p=1,
         frequency_penalty=1,
         presence_penalty=1
-    )
+        )
 
-    result = translator.translate_text(text=response.choices[0].text, source_lang="HU", target_lang="EN-US")
-    
-    return result.text;
+        result = translator.translate_text(text=response.choices[0].text, source_lang="HU", target_lang="EN-US")
+        
+        messages.append(result.text)
+        print(result.text)
+        
+    return render_template('my-form.html', messages=messages)
+
 
 
 if __name__ == "__main__":
