57925ba96552e05136212b44bb52bbccc728ab4c
Hajnal Mate <matehajnal.hajnal@gmail.com>
2023-02-25 22:19:06 +0100

    base of the webapp

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+import openai
+import deepl
+
+openai.api_key = BASSZUS
+auth_key = KULCS
+translator = deepl.Translator(auth_key)
+
+app = Flask(__name__)
+
+""" @app.route('/')
+def index():
+    return render_template('index.html') """
+
+@app.route('/')
+def my_form():
+    return render_template('my-form.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=text,
+        temperature=0.6,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=1,
+        presence_penalty=1
+    )
+
+    result = translator.translate_text(text=response.choices[0].text, source_lang="HU", target_lang="EN-US")
+    
+    return result.text;
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
